d1d3bac5c42913bff93a13b5b23177708db0814a
rysk1013 <rysk2908@gmail.com>
2021-06-03 02:03:18 +0900

    add packages

--- main.go
+++ main.go
@@ -1,5 +1,17 @@
 package main
 
-func main() {
+import (
+	"fmt"
 	
+	"lessons2/mylib"
+	"lessons2/mylib/under"
+)
+
+func main() {
+	s := []int{1, 2, 3, 4, 5}
+	fmt.Println(mylib.Average(s))
+
+	mylib.Say()
+
+	under.Hello()
 }
